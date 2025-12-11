89ac4e4c8dc93b3d7a7e485d8f3a33b17bb26d51
Chang Liu (Enterprise Products) <9713593+chang-l@users.noreply.github.com>
2025-12-11 13:10:19 -0800

    Address comment and created a tokenzier dir
    Signed-off-by: Chang Liu (Enterprise Products) <9713593+chang-l@users.noreply.github.com>

--- tensorrt_llm/inputs/utils.py
+++ tensorrt_llm/inputs/utils.py
@@ -23,7 +23,6 @@ from tensorrt_llm.inputs.multimodal import (MultimodalServerConfig,
                                             default_hasher)
 from tensorrt_llm.inputs.registry import (MULTIMODAL_PLACEHOLDER_REGISTRY,
                                           MultimodalPlaceholderPlacement)
-from tensorrt_llm.llmapi.deepseek_v32 import DeepseekV32Tokenizer
 from tensorrt_llm.llmapi.llm_utils import ModelLoader
 from tensorrt_llm.tokenizer import TokenizerBase, TransformersTokenizer
 from tensorrt_llm.tokenizer.deepseek_v32 import DeepseekV32Tokenizer
