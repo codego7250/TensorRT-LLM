a89fc40ae3eced195e0d22cbcde41c068ef5e977
codego7250 <codego7250@gmail.com>
2026-01-22 05:56:43 +0000

    ok

--- tensorrt_llm/sampling_params.py
+++ tensorrt_llm/sampling_params.py
@@ -362,7 +362,6 @@ class SamplingParams:
             # kimi_k2 model uses the eos_token_id in generation config
             if (
                 hf_model_config is not None
-                and hf_model_config.model_type == "deepseek_v3"
                 and generation_config is not None
                 and isinstance(generation_config.eos_token_id, int)
             ):
